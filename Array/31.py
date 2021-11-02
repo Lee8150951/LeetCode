27f192af7a548f2102c29a6f9f8a873135628f1a
Lee8150951 <Liys9875@outlook.com>
2021-11-02 10:47:06 +0800

    day59

---
+++ Array/31.py
@@ -0,0 +1,24 @@
+from typing import List
+class Solution:
+    def nextPermutation(self, nums: List[int]) -> None:
+        i = len(nums) - 2
+        while i >= 0 and nums[i] >= nums[i + 1]:
+            i -= 1
+        if i >= 0:
+            j = len(nums) - 1
+            while j >= 0 and nums[i] >= nums[j]:
+                j -= 1
+            nums[i], nums[j] = nums[j], nums[i]
+        
+        left, right = i + 1, len(nums) - 1
+        while left < right:
+            nums[left], nums[right] = nums[right], nums[left]
+            left += 1
+            right -= 1
+        
+
+if __name__=="__main__":
+  nums = [1, 2, 3]
+  method = Solution()
+  answer = method.nextPermutation(nums)
+  print(answer)
